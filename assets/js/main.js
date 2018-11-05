1e8ac362b170936d22e6a307426c919427cf5a42
Alecto <fomenkoandrey@inbox.ru>
2018-11-05 16:07:52 +0200

    Добавляем в скрипт код для работы карусели.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,40 @@
         $(this).toggleClass('active').next().toggleClass('active');
     });
 
+    let $slideItems   = $('.carousel-slide'),
+        $nextBtn      = $('.next.carousel-control'),
+        $prevBtn      = $('.prev.carousel-control'),
+        currentSlide  = 0;
+
+    let gotoSlide = (n) => {
+        $slideItems[currentSlide].classList.toggle('active');
+        currentSlide = (n + $slideItems.length) % $slideItems.length;
+        $slideItems[currentSlide].classList.toggle('active');
+    };
+
+    let nextSlide = () => {
+        gotoSlide(currentSlide + 1);
+    };
+
+    let prevSlide = () => {
+        gotoSlide(currentSlide - 1);
+    };
+
+    let nextClickHandler = () => {
+        nextSlide();
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 5000);
+    };
+
+    let prevClickHandler = () => {
+        prevSlide();
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 5000);
+    };
+
+    let slideInterval = setInterval(nextSlide, 5000);
+
+    $nextBtn.on('click', nextClickHandler);
+    $prevBtn.on('click', prevClickHandler);
+
 })(jQuery);

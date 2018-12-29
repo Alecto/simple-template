c852af244d35ca3c7df8845f408438d0c94578e4
Alecto <fomenkoandrey@inbox.ru>
2018-12-30 00:02:05 +0200

    Исправление замечаний ESLint.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,41 @@
-(function ($) {
+$('.menu__btn').on('click', function () {
+    $(this).toggleClass('active').
+        next().
+        toggleClass('active');
+});
 
-    $('.menu__btn').on('click', function () {
-        $(this).toggleClass('active').next().toggleClass('active');
-    });
+let $slideItems = $('.carousel-slide');
+let $nextBtn = $('.next.carousel-control');
+let $prevBtn = $('.prev.carousel-control');
+let currentSlide = 0;
 
-    let $slideItems   = $('.carousel-slide'),
-        $nextBtn      = $('.next.carousel-control'),
-        $prevBtn      = $('.prev.carousel-control'),
-        currentSlide  = 0;
+let gotoSlide = (n) => {
+    $slideItems[currentSlide].classList.toggle('active');
+    currentSlide = (n + $slideItems.length) % $slideItems.length;
+    $slideItems[currentSlide].classList.toggle('active');
+};
 
-    let gotoSlide = (n) => {
-        $slideItems[currentSlide].classList.toggle('active');
-        currentSlide = (n + $slideItems.length) % $slideItems.length;
-        $slideItems[currentSlide].classList.toggle('active');
-    };
+let nextSlide = () => {
+    gotoSlide(currentSlide + 1);
+};
 
-    let nextSlide = () => {
-        gotoSlide(currentSlide + 1);
-    };
+let prevSlide = () => {
+    gotoSlide(currentSlide - 1);
+};
 
-    let prevSlide = () => {
-        gotoSlide(currentSlide - 1);
-    };
+let slideInterval = setInterval(nextSlide, 5000);
 
-    let nextClickHandler = () => {
-        nextSlide();
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, 5000);
-    };
+let nextClickHandler = () => {
+    nextSlide();
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 5000);
+};
 
-    let prevClickHandler = () => {
-        prevSlide();
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, 5000);
-    };
+let prevClickHandler = () => {
+    prevSlide();
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 5000);
+};
 
-    let slideInterval = setInterval(nextSlide, 5000);
-
-    $nextBtn.on('click', nextClickHandler);
-    $prevBtn.on('click', prevClickHandler);
-
-})(jQuery);
+$nextBtn.on('click', nextClickHandler);
+$prevBtn.on('click', prevClickHandler);
